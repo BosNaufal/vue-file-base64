26cfe633559eceafd2128663e542d8afbb7855f6
Thijs van den Anker <thijs@bitsnbolts.nl>
2019-01-22 12:40:19 +0100

    Upgrade to webpack-4

---
+++ src/components/app.vue
@@ -0,0 +1,56 @@
+
+<template>
+  <div align="center">
+    <h1>Vue, File To Base64 Converter</h1>
+    <p>*) Try To Upload Some Image~</p>
+
+    <file-base64
+      :multiple="true"
+      :done="getFiles"
+    />
+
+    <div class="text-center">
+      <img
+        v-for="img in files"
+        :key="img.name"
+        alt=""
+        :src="img.base64"
+      >
+    </div>
+
+    <div v-if="files.length != 0">
+      <h3 class="text-center mt-25">
+        Callback Object
+      </h3>
+      <div
+        class="pre-container"
+        align="left"
+      >
+        <pre>{{ files }}</pre>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import fileBase64 from './vue-file-base64.vue'
+
+export default {
+
+  components: { fileBase64 },
+
+  data () {
+    return {
+      files: []
+    }
+  },
+
+  methods: {
+    getFiles (files) {
+      this.files = files
+    }
+  }
+}
+
+</script>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import App from './components/app.vue';
+import Vue from 'vue'
+import App from './components/app.vue'
 
 Vue.config.debug = true
 Vue.config.devtools = true
